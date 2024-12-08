bb810e9b1b7fad6aed6d5678d444431ffbdc72a0
Channa Dissanayaka <ywchanna@gmail.com>
2024-12-09 00:10:52 +0530

    modified

--- model.py
+++ model.py
@@ -1,29 +1,62 @@
 from mesa import Model
 from mesa.time import RandomActivation
 from mesa.space import MultiGrid
-from agents import AnimalAgent, PatrolAgent, ThreatAgent
+from agents import AnimalAgent, PatrolAgent, HunterAgent, FoodAgent, WaterAgent
 
-class ForestModel(Model):
-    def __init__(self, width, height, num_animals, num_patrols, num_threats):
-        super().__init__()  # Ensure the base class is initialized
-        self.grid = MultiGrid(width, height, torus=True)
+class MultiAgentModel(Model):
+    def __init__(self, width, height, num_animals, num_patrols, num_hunters):
+        super().__init__()
+        self.width = width
+        self.height = height
+        self.grid = MultiGrid(width, height, torus=False)
         self.schedule = RandomActivation(self)
 
-        # Add agents to the grid and schedule
-        for i in range(num_animals):
-            animal = AnimalAgent(i, self)
-            self.grid.place_agent(animal, (self.random.randrange(width), self.random.randrange(height)))
+        # Randomly locate water agents(static)
+        for _ in range(200):
+            x = self.random.randrange(width)
+            y = self.random.randrange(height)
+            water = WaterAgent(self.next_id(), self)
+            self.grid.place_agent(water, (x, y))
+            self.schedule.add(water)
+
+        # Randomly locate food agents(static)
+        for _ in range(200):
+            x = self.random.randrange(width)
+            y = self.random.randrange(height)
+            food = FoodAgent(self.next_id(), self)
+            self.grid.place_agent(food, (x, y))
+            self.schedule.add(food)
+              
+
+        # Add Animal Agents
+        for _ in range(num_animals):
+            x = self.random.randrange(width)
+            y = self.random.randrange(height)
+            animal = AnimalAgent(self.next_id(), self)
+            self.grid.place_agent(animal, (x, y))
             self.schedule.add(animal)
 
-        for i in range(num_patrols):
-            patrol = PatrolAgent(num_animals + i, self)
-            self.grid.place_agent(patrol, (self.random.randrange(width), self.random.randrange(height)))
+         # Add Patrol Agents
+        for _ in range(num_patrols):
+            x = self.random.randrange(width)
+            y = self.random.randrange(height)
+            patrol = PatrolAgent(self.next_id(), self)
+            self.grid.place_agent(patrol, (x, y))
             self.schedule.add(patrol)
+        
+        # Add Hunter Agents
+        for _ in range(num_hunters):
+            x = self.random.randrange(width)
+            y = self.random.randrange(height)
+            hunter = HunterAgent(self.next_id(), self)
+            self.grid.place_agent(hunter, (x, y))
+            self.schedule.add(hunter)
+    
+    def random_position(self):
+        """Get a random position within the grid."""
+        return self.random.randrange(self.grid.width), self.random.randrange(self.grid.height)
 
-        for i in range(num_threats):
-            threat = ThreatAgent(num_animals + num_patrols + i, self)
-            self.grid.place_agent(threat, (self.random.randrange(width), self.random.randrange(height)))
-            self.schedule.add(threat)
 
     def step(self):
+        """Advance the model by one step."""
         self.schedule.step()
